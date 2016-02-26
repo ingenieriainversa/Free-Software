3994a8fa4e4de0c3bb781da642c15269f4085d5d
100% Free Software <jdg@member.fsf.org>
2016-02-26 18:03:38 +0100

    Update ifms.h

--- Integrated-Flight-Management-System/ifms.h
+++ Integrated-Flight-Management-System/ifms.h
@@ -1,11 +1,33 @@
+/*
+ * Integrated Flight Management System v0.01
+ * ifms.h
+ * Copyleft - 2014  Javier Dominguez Gomez
+ * Written by Javier Dominguez Gomez <jdg@member.fsf.org>
+ * GnuPG Key: 6ECD1616
+ * Madrid, Spain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 /* Definicion de constantes */
-const int max_char_codigo = 6;	// Numero maximo de caracteres para el codigo de vuelo.
-const int max_char_hora = 5;	// Numero maximo de caracteres para la hora.
-const int max_char_mes = 2;	// Numero maximo de caracteres para el mes.
-const int max_meses = 24;	// Numero maximo de meses.
-const int max_vuelos_mes = 50;	// Numero maximo de vuelos por cada mes.
+const int max_char_codigo = 6;				 // Numero maximo de caracteres para el codigo de vuelo.
+const int max_char_hora = 5;				 // Numero maximo de caracteres para la hora.
+const int max_char_mes = 2;				 // Numero maximo de caracteres para el mes.
+const int max_meses = 24;				 // Numero maximo de meses.
+const int max_vuelos_mes = 50;				 // Numero maximo de vuelos por cada mes.
 const int max_vuelos_total = max_meses * max_vuelos_mes; // Numero maximo de vuelos en total.
 
 /* Definicion de variables globales */
@@ -139,7 +161,7 @@ int ContadorMesesNoDisponiblesAnno(int i) {
 	return meses_no_disponibles;
 }
 
-/* FuciÛn para control de errores generales en el anno. */
+/* Fucion para control de errores generales en el anno. */
 bool CompruebaAnno(int i) {
 	bool resultado = true;
 
