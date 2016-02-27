c37c6ccf8dca366075b5b6a1862637bae92c85f3
100% Free Software <jdg@member.fsf.org>
2016-02-27 16:29:09 +0100

    Add some methods to ilms.java

--- Integrated-Library-Management-System/ilms.java
+++ Integrated-Library-Management-System/ilms.java
@@ -22,5 +22,69 @@ public class Library
     public String getName(){return name;}
     public String getAddress(){return address;}
     public int getNumEmployees(){return numEmployees;}
+    public int getNumUsers(){return users.size();}
+    public int getNumItems(){return items.size();}
     public boolean getOpen(){return open;}
+    
+    public void listUser(int index)
+    {
+        if(index >= 0 && index < users.size()) {
+            String user = users.get(index);
+            System.out.println(user);
+        }
+    }
+    
+    public void listAllUsers()
+    {
+        for(String user : users) {
+            System.out.println(user);
+        }
+    }
+    
+    public void listUserMatching(String searchString)
+    {
+        for(String user : users) {
+            if(user.contains(searchString)) {
+                System.out.println(user);
+            }
+        }
+    }
+    
+    public void removeUser(int index)
+    {
+        if(index >= 0 && index < users.size()) {
+            users.remove(index);
+        }
+    }
+    
+    public void listItem(int index)
+    {
+        if(index >= 0 && index < items.size()) {
+            String item = items.get(index);
+            System.out.println(item);
+        }
+    }
+    
+    public void listAllItems()
+    {
+        for(String item : items) {
+            System.out.println(item);
+        }
+    }
+    
+    public void listItemMatching(String searchString)
+    {
+        for(String item : items) {
+            if(item.contains(searchString)) {
+                System.out.println(item);
+            }
+        }
+    }
+    
+    public void removeItem(int index)
+    {
+        if(index >= 0 && index < items.size()) {
+            items.remove(index);
+        }
+    }
 }
