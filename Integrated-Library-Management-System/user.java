ade90992435b26e551519e753a8bb56394cccad0
100% Free Software <jdg@member.fsf.org>
2016-02-28 12:43:52 +0100

    Create user.java

---
+++ Integrated-Library-Management-System/user.java
@@ -0,0 +1,20 @@
+public class User
+{
+  private String name;
+  private String Surname;
+  private int age;
+  private boolean active;
+  
+  public User(String name, String Surname, int age)
+  {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    active = true;
+  }
+  
+  public String getUserName(){return name;}
+  public String getUserSurname(){return surname;}
+  public int getUserAge(){return age;}
+  public boolean getUserActive(){return active;}
+}
