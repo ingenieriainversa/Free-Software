7b6c7a4de13aa83f9d36e85dff46165c9a8d1fd5
Ingenieria.inversa <jdg@member.fsf.org>
2016-12-01 16:16:55 +0100

    Create Main.java

---
+++ IntegratedApplicationServerSystem/Main.java
@@ -0,0 +1,44 @@
+/*
+ * Integrated Application Server System v0.01
+ * Main.java
+ * Copyleft - 2016  Javier Dominguez Gomez
+ * Written by Javier Dominguez Gomez <jdg@member.fsf.org>
+ * GnuPG Key: 6ECD1616
+ * Madrid, Spain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main;
+
+import was.Was;
+
+public class Main {
+	
+	private static Was var;
+	
+	public static void main(String[] args) {
+		setVar(new Was());
+		var.printData();
+	}
+
+	public static Was getVar() {
+		return var;
+	}
+
+	public static void setVar(Was var) {
+		Main.var = var;
+	}
+
+}
