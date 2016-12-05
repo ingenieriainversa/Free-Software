100e038f26f9fbb5a2c29baf2b35813de12ce40e
Ingenieria.inversa <jdg@member.fsf.org>
2016-12-05 17:39:38 +0100

    Create FileExplorer.java

---
+++ Application-Server-Discovery/FileExplorer.java
@@ -0,0 +1,43 @@
+/*
+ * Application Server Discovery v0.01
+ * FileExplorer.java
+ * Copyleft - 2016  Javier Dominguez Gomez
+ * Written by Javier Dominguez Gomez <jdg@member.fsf.org>
+ * GnuPG Key: 6ECD1616
+ * Madrid, Spain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main;
+
+import java.io.File;
+
+public class FileExplorer {
+
+	public void search(String path, String file) {
+
+		File root = new File(path);
+		File[] list = root.listFiles();
+
+		if (list == null)
+			return;
+
+		for (File f : list) {
+			if (f.isFile() && f.getName().equals(file)) {
+				System.out.println("File:" + f.getAbsoluteFile());
+			}
+		}
+	}
+}
