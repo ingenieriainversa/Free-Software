5ec699ef411ce77d9f2add4f86a4d536526ef739
Ingenieria.inversa <jdg@member.fsf.org>
2016-12-21 22:37:32 +0100

    Update Main.java

--- Application-Server-Discovery/Main.java
+++ Application-Server-Discovery/Main.java
@@ -53,60 +53,60 @@ public class Main {
 	private static String outputFormat;
 	private static CommandLineParser parser;
 	private static CommandLine cmdLine;
-	
+
 	public static void main(String[] args) {
-		
+
 		// Set required options to null
 		mode = null;
 		wasHome = null;
 		final String DEFAULT_FORMAT = "table";
 		outputFormat = DEFAULT_FORMAT;
-		
+
 		// New instance of Options class
 		Options options = new Options();
-		
+
 		// All options: name, alias, required and help text
 		options.addOption("h", "help", false, "Print help");
 		options.addOption("wasHome", true, "WAS installation path");
 		options.addOption("mode", true, "Output mode");
-		
+
 		// Both options can not be displayed simultaneously.
-        OptionGroup group =  new OptionGroup();  
-        group.addOption(new Option("csv", "Output in CSV format"));  
-        group.addOption(new Option("table", "Output in table format"));  
-        options.addOptionGroup(group);
-        
+		OptionGroup group = new OptionGroup();
+		group.addOption(new Option("csv", "Output in CSV format"));
+		group.addOption(new Option("table", "Output in table format"));
+		options.addOptionGroup(group);
+
 		try {
 			parser = new DefaultParser();
 			cmdLine = parser.parse(options, args);
-			
+
 			// Option -h or --help
 			if (cmdLine.hasOption("h")) {
 				new HelpFormatter().printHelp(Main.class.getCanonicalName(), options);
 				return;
 			}
-			
+
 			// Option -wasHome
 			wasHome = cmdLine.getOptionValue("wasHome");
 			if (wasHome == null) {
 				throw new org.apache.commons.cli.ParseException("wasHome option is required");
 			}
-			
+
 			// Option -mode
 			mode = cmdLine.getOptionValue("mode");
 			if (mode == null) {
 				throw new org.apache.commons.cli.ParseException("mode option is required");
 			}
-			
+
 			// Options -csv and -table for output format
-			if (cmdLine.hasOption("csv")){
-                outputFormat = "csv";
-            } else if (cmdLine.hasOption("table")){  
-            	outputFormat = "table";
-            } else {
-            	outputFormat = null;
-            }
-			
+			if (cmdLine.hasOption("csv")) {
+				outputFormat = "csv";
+			} else if (cmdLine.hasOption("table")) {
+				outputFormat = "table";
+			} else {
+				outputFormat = null;
+			}
+
 		} catch (org.apache.commons.cli.ParseException ex) {
 			System.out.println(ex.getMessage());
 			new HelpFormatter().printHelp(Main.class.getCanonicalName(), options);
@@ -115,67 +115,65 @@ public class Main {
 			new HelpFormatter().printHelp(Main.class.getCanonicalName(), options);
 			System.exit(1);
 		}
-		
+
 		// New instance of WasProductParser class
 		wasProduct = new WasProductParser();
 		// Parse WAS.product file
 		wasProduct.parse(wasHome);
 		// Get WAS product data
 		wasProductData = wasProduct.getWasProduct();
-		
-		
+
 		// New instance of ProfileRegistryParser class
 		profileRegistryXml = new ProfileRegistryParser();
 		// Parse profileRegistry.xml file
 		profileRegistryXml.parse(wasHome);
 		// Get Profiles ArrayList
 		profiles = profileRegistryXml.getProfiles();
-		
-		
+
 		// New instance of Was class
 		was = new Was(wasProductData, profiles);
-		
-		if(mode.equals("productData")) {
+
+		if (mode.equals("productData")) {
 			System.out.println("Product data:");
 			// Print all product data
 			was.printWasProductData();
 		}
-		
-		if(mode.equals("profileList")) {
+
+		if (mode.equals("profileList")) {
 			System.out.println("\nProfile list:");
 			// Print a profile list
 			was.printProfileList();
 		}
-		
-		
-		/* For each profile you can get, set or print
-		 * all data that you want about it.
+
+		/*
+		 * For each profile you can get, set or print all data that you want
+		 * about it.
 		 */
 		int profileIndex = 0;
-		while(profileIndex < was.getProfiles().size()) {
-			
+		while (profileIndex < was.getProfiles().size()) {
+
 			// Get the profile
 			Profile profile = was.getProfiles().get(profileIndex);
-			
+
 			// New instance of ServerindexParser class
 			serverindexXml = new ServerindexParser();
-			
+
 			// Get the serverindex.xml absolute path
 			String serverindexFile = profile.getServerindex();
-			
+
 			// Parse serverindex.xml file
 			serverindexXml.parse(serverindexFile);
-			
+
 			// Get Jvms ArrayList
 			jvms = serverindexXml.getJvms();
-			
+
 			// For each profile set the jvm ArrayList
 			profile.setJvms(jvms);
-			
+
 			// For example, print the jvm name list
-//			System.out.println("\n"+profile.getName()+" Jvm list:");
-//			profile.printJvmList();
-			
+			// System.out.println("\n"+profile.getName()+" Jvm list:");
+			// profile.printJvmList();
+
 			++profileIndex;
 		}
 	}
